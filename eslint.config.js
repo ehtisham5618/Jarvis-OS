daf4a0988391bbff2a9509b645b94d0b073b4b50
ehtisham5618 <ehtisham.malik5618@gmail.com>
2026-07-21 23:09:00 +0500

    fix(ci): disable ban-ts-comment rule

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,7 @@ export default tseslint.config(
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   },
   eslintPluginPrettier,
